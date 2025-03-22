740a0463036d0e1f95984a4707bea8bd732b1373
Sirered <gikurniawan.1995@gmail.com>
2025-03-22 11:05:26 +0700

    init

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.3"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "Anuisa-website"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudGcpVersion"] = "6.0.1"
+extra["springCloudVersion"] = "2024.0.0"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/Anuisa_website/prototype/PrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package Anuisa_website.prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
