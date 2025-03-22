3a6970dcd299314acdfdcba5b297590568f1ae36
TomoriNow <seanarcher1974@gmail.com>
2025-03-22 15:24:36 +1100

    [CHORE] initialize model and repository

---
+++ src/main/java/Anuisa_website/prototype/model/Profile.java
@@ -0,0 +1,41 @@
+package Anuisa_website.prototype.repository;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.Instant;
+
+@Entity
+@Table(name = "Profile")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Profile {
+    @Id
+    private String uniId;
+    
+    @Column(nullable = false)
+    private Instant timestamp;
+    
+    @Column(nullable = false, unique = true)
+    private String email;
+    
+    @Column(nullable = false)
+    private String fullName;
+    
+    private String nickname;
+    private String degree;
+    private String college;
+    private String school;
+    private String program;
+    private String intake;
+    private String otherDegrees;
+    private String researchProject;
+    private String linkedIn;
+    private String instagram;
+    private String portfolio;
+    private String expertise;
+    private String careerInterests;
+    private String profilePicture;
+    private String bio;
+}
