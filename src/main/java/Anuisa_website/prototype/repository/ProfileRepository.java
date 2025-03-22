0efefd8a897a220b2be9ee3c49b87c9611715050
TomoriNow <seanarcher1974@gmail.com>
2025-03-22 15:28:33 +1100

    [CHORE] added repo init

---
+++ src/main/java/Anuisa_website/prototype/repository/ProfileRepository.java
@@ -0,0 +1,8 @@
+package Anuisa_website.prototype.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile, Long> {
+    Profile findByEmail(String email);
+}
